acc6a60783d2a5f3181d44e0147ec1420443b904
Joel Larson <519553+JoelLarson@users.noreply.github.com>
2022-03-11 19:50:40 -0700

    Update development container to use correct keycloak version

--- run.sh
+++ run.sh
@@ -3,5 +3,6 @@
 docker run -p 8080:8080 \
   -e KEYCLOAK_ADMIN=admin \
   -e KEYCLOAK_ADMIN_PASSWORD=admin \
-  quay.io/keycloak/keycloak:17.0.0 \
+  --name denhac \
+  quay.io/keycloak/keycloak-x:latest \
   start-dev
